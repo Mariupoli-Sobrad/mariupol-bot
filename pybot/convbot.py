1ffb5c2e6bbaffb21c173c0573ef0ce21b1f2f15
Alexey Boriskin <uruz@yandex-team.ru>
2022-12-17 16:18:27 +0400

    fix comma

--- pybot/convbot.py
+++ pybot/convbot.py
@@ -717,8 +717,7 @@ def main() -> None:
                 MessageHandler(PRIVATE_TEXT, gifts_parent_result),
             ]
         },
-        fallbacks=[CommandHandler('finish', finish),
-                   CommandHandler('start', start)]
+        fallbacks=[CommandHandler('finish', finish), CommandHandler('start', start)],
         per_chat=False,
         name="conv_handler",
         persistent=True
